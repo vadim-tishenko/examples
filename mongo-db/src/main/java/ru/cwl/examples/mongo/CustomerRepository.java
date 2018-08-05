19949daa586078d20bd6a29f6697980a88375b4b
Vadim Tishenko <vad.tishenko@gmail.com>
2018-08-06 00:00:11 +0300

    add mongo example

---
+++ mongo-db/src/main/java/ru/cwl/examples/mongo/CustomerRepository.java
@@ -0,0 +1,16 @@
+package ru.cwl.examples.mongo;
+
+/**
+ * Created by vadim.tishenko
+ * on 05.08.2018 23:48.
+ */
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+
+}
