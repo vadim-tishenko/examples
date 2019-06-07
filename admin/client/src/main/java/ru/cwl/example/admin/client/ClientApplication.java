e49188527482795407c2f5fb86cb56f316b07fda
Vadim Tishenko <tishenko@advantum.pro>
2019-06-07 12:04:49 +0300

    add sb admin example

---
+++ admin/client/src/main/java/ru/cwl/example/admin/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package ru.cwl.example.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
---
+++ admin/server/src/main/java/ru/cwl/example/admin/server/ServerApplication.java
@@ -0,0 +1,17 @@
+package ru.cwl.example.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
