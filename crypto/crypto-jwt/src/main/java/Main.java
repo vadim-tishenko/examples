39c342f4dfd5363a59d27667304bbe63e2c80357
Vadim Tishenko <tishenko@advantum.pro>
2019-08-29 18:10:10 +0300

    add jwt example

---
+++ crypto/crypto-jwt/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.security.KeyPair;
+
+public class Main {
+    public static void main(String[] args) {
+//        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+//        Key key2 = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        String jws = Jwts.builder()
+                .setSubject("Joe")
+                .claim("c1","value1")
+                .claim("c2","value2")
+                .signWith(keyPair.getPrivate()).compact();
+
+        System.out.println(jws);
+
+        Jws<Claims> res = Jwts.parser().setSigningKey(keyPair.getPublic()).parseClaimsJws(jws);
+        Object c1 = res.getBody().get("c1");
+        Object c2 = res.getBody().get("c2");
+        String subj = res.getBody().getSubject();
+
+        System.out.printf("c1:%s c2:%s subj:%s\n",c1,c2,subj);
+    }
+}
