4de7f27d37923cbb1f8ccb863fcccd84d0b11882
Vadim Tishenko <tishenko@advantum.pro>
2019-08-30 17:39:00 +0300

    add load keyPair from disk

--- crypto/crypto-jwt/src/main/java/Main.java
+++ crypto/crypto-jwt/src/main/java/Main.java
@@ -1,31 +1,37 @@
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
-import java.security.Key;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 /**
-   https://jwt.io/
-   https://github.com/jwtk/jjwt
-
- https://www.cryptosys.net/pki/rsakeyformats.html
- https://help.globalscape.com/help/archive/eft6-2/mergedprojects/eft/server_ssh_key_formats.htm
- https://stackoverflow.com/questions/12749858/rsa-public-key-format
-
- https://stackoverflow.com/questions/11410770/load-rsa-public-key-from-file
-
+ * https://jwt.io/
+ * https://github.com/jwtk/jjwt
+ * <p>
+ * https://www.cryptosys.net/pki/rsakeyformats.html
+ * https://help.globalscape.com/help/archive/eft6-2/mergedprojects/eft/server_ssh_key_formats.htm
+ * https://stackoverflow.com/questions/12749858/rsa-public-key-format
+ * <p>
+ * https://stackoverflow.com/questions/11410770/load-rsa-public-key-from-file
  */
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+//        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+        KeyPair keyPair = getKeyPairFromDisk();
 //        Key key2 = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
         String jws = Jwts.builder()
                 .setSubject("Joe")
-                .claim("c1","value1")
-                .claim("c2","value2")
+                .claim("c1", "value1")
+                .claim("c2", "value2")
                 .signWith(keyPair.getPrivate()).compact();
 
         System.out.println(jws);
@@ -35,6 +41,21 @@ public class Main {
         Object c2 = res.getBody().get("c2");
         String subj = res.getBody().getSubject();
 
-        System.out.printf("c1:%s c2:%s subj:%s\n",c1,c2,subj);
+        System.out.printf("c1:%s c2:%s subj:%s\n", c1, c2, subj);
+
+//        KeyPair kp = getKeyPairFromDisk();
+//
+//        kp.toString();
+
+    }
+
+    private static KeyPair getKeyPairFromDisk() throws Exception {
+        byte[] privateBytes = Files.readAllBytes(Paths.get(Main.class.getClassLoader().getResource("keys/private_key.der").toURI()));
+        byte[] publicBytes = Files.readAllBytes(Paths.get(Main.class.getClassLoader().getResource("keys/public_key.der").toURI()));
+
+        PrivateKey privateKey = KeyUtils.getPrivateKey(privateBytes);
+        PublicKey publicKey = KeyUtils.getPublicKey(publicBytes);
+
+        return new KeyPair(publicKey, privateKey);
     }
 }
